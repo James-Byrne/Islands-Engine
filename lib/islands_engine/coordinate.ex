83ba3dd48db6e5cd483e0636b3cd08c424a653fd
James Byrne <jamesdesbyrne@gmail.com>
2017-04-03 12:37:56 +0100

    Adding comments to coordinate and island

--- lib/islands_engine/coordinate.ex
+++ lib/islands_engine/coordinate.ex
@@ -3,18 +3,22 @@ defmodule IslandsEngine.Coordinate do
 
   alias IslandsEngine.Coordinate
 
+  # Start a link with a new Agent whose state is a Coordinate instance
   def start_link() do
     Agent.start_link(fn -> %Coordinate{} end)
   end
 
+  # Check if the coordinate has been guessed (guessed will be true | false)
   def guessed?(coordinate) do
     Agent.get(coordinate, fn state -> state.guessed? end)
   end
 
+  # Check if coordinate is in an island (returns :none or island name (as an atom))
   def island(coordinate) do
     Agent.get(coordinate, fn state -> state.in_island end)
   end
 
+  # Check if the coordinate is in an island and return a bool
   def in_island?(coordinate) do
     case Agent.get(coordinate, fn state -> state.in_island end) do
       :none -> false
@@ -22,18 +26,22 @@ defmodule IslandsEngine.Coordinate do
     end
   end
 
+  # Check if the coordinate has been hit
   def hit?(coordinate) do
     Coordinate.in_island?(coordinate) && Coordinate.guessed?(coordinate)
   end
 
+  # Guess the coordinate (Updates Coordinate guessed to true)
   def guess(coordinate) do
     Agent.update(coordinate, fn state -> Map.put(state, :guessed?, true) end)
   end
 
+  # Set the current coordinate as within an island
   def set_in_island(coordinate, value) when is_atom value do
     Agent.update(coordinate, fn state -> Map.put(state, :in_island, value) end)
   end
 
+  # Return the coordinate as a string
   def to_string(coordinate) do
     "(in_island:#{in_island?(coordinate)}, guessed:#{guessed?(coordinate)})"
   end
--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -1,19 +1,24 @@
 defmodule IslandsEngine.Island do
   alias IslandsEngine.Coordinate
 
+  # Start a link with a new Island Agent whose initial state is an empty list
   def start_link do
     Agent.start_link(fn -> [] end)
   end
 
+  # Replace the coordinates of the island with anew list of coords
   def replace_coordinates(island, new_coords) when is_list new_coords do
     Agent.update(island, fn _state -> new_coords end)
   end
 
+  # Check if the island has a palm tree
+  # Check if all the islands coordinates have been hit
   def forested?(island) do
     Agent.get(island, fn state -> state end)
     |> Enum.all?(fn coord -> Coordinate.hit?(coord) === true end)
   end
 
+  # Print the islands current state as a string
   def to_string(island) do
     string = Agent.get(island, fn state -> state end)
     |> Enum.reduce("", fn coord, acc -> "#{acc}, #{Coordinate.to_string(coord)}" end)
